0da1c5e39410a6aa090224b3f8cfb15656f72797
Ouedraogo ousmane <generaloks496@gmail.com>
2022-09-22 09:45:53 +0000

    heroku-deploy ending

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+
+app.use(express.static(__dirname + '/dist/transport-app2022'));
+
+app.get('/*', function(req, res) {
+
+    res.sendFile(path.join(__dirname + '/dist/transport-app2022/index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
